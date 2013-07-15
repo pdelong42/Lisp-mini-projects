5587cb75356ae6872ae185bea2ee92f49b4ba591
Paul DeLong <pd72@nyu.edu>
2013-07-15 10:50:10 -0400

    made both recursive and iterative versions of each function active;

--- prime.lisp
+++ prime.lisp
@@ -1,5 +1,3 @@
-#!/usr/bin/clisp
-
 (defun even?
    (x)
    (equal (mod x 2) 0)
@@ -14,35 +12,31 @@
    )
 )
 
-;; defined iteratively
-;
-;(defun prime?
-;   (x)
-;   (do
-;      ((i 2 (+ i 1)))
-;      ((> i (midpoint x)))
-;      (if
-;         (equal 0 (mod x i))
-;         (return-from prime?)
-;      )
-;   )
-;   t
-;)
+(defun prime?-iterative
+   (x)
+   (do
+      (  (i 2 (+ i 1)))
+      (  (> i (midpoint x)))
+      (if
+         (equal 0 (mod x i))
+         (return-from prime?-iterative)
+      )
+   )
+   t
+)
 
-; defined recursively
-
-(defun prime?
+(defun prime?-recursive
    (x)
    (labels
       (  (subrange
             (y)
             (unless
                (> y 1)
-               (return-from prime? t)
+               (return-from prime?-recursive t)
             )
             (unless
                (> (mod x y) 0)
-               (return-from prime?)
+               (return-from prime?-recursive)
             )
             (subrange (- y 1))
          )
@@ -51,31 +45,27 @@
    )
 )
 
-;; defined iteratively
-;
-;(defun factors
-;   (x)
-;   (if
-;      (prime? x)
-;      (return-from factors (list x))
-;   )
-;   (do
-;      ((i 2 (+ i 1)))
-;      ((>= i x))
-;      (unless
-;         (> (mod x i) 0)
-;         (return-from factors (cons i (factors (/ x i))))
-;      )
-;   )
-;)
-
-; defined recursively
-
-(defun factors
+(defun factors-iterative
    (x)
    (if
       (prime? x)
-      (return-from factors (list x))
+      (return-from factors-iterative (list x))
+   )
+   (do
+      (  (i 2 (+ i 1)))
+      (  (>= i x))
+      (unless
+         (> (mod x i) 0)
+         (return-from factors-iterative (cons i (factors (/ x i))))
+      )
+   )
+)
+
+(defun factors-recursive
+   (x)
+   (if
+      (prime? x)
+      (return-from factors-recursive (list x))
    )
    (labels
       (  (nexttry
@@ -86,7 +76,7 @@
             )
             (unless
                (> (mod x y) 0)
-               (return-from factors (cons y (factors (/ x y))))
+               (return-from factors-recursive (cons y (factors (/ x y))))
             )
             (nexttry (+ y 1))
          )
@@ -95,11 +85,14 @@
    )
 )
 
+(defun prime?  (x) (prime?-iterative  x))
+(defun factors (x) (factors-iterative x))
+
 ;(format t "~A~%" (even? (read)))
 ;(format t "~A~%" (prime? (read)))
 
-(do
-   ((i 1 (+ i 1)))
-   ((>= i 100))
-   (format t "~A ~A~%" i (factors i))
-)
+;(do
+;   ((i 1 (+ i 1)))
+;   ((>= i 100))
+;   (format t "~A ~A~%" i (factors i))
+;)
